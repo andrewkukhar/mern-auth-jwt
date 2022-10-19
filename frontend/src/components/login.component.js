ba5492ffa018985311cc8f954cab352d015a8d0f
andreyrise <andrey.rise@gmail.com>
2022-10-18 19:03:17 -0700

    fix all errors

--- frontend/src/components/login.component.js
+++ frontend/src/components/login.component.js
@@ -1,21 +1,9 @@
-import Form from "react-validation/build/form";
-import Input from "react-validation/build/input";
-import CheckButton from "react-validation/build/button";
 import React, { Component } from "react";
 
 import AuthService from "../services/auth.service";
 
 import { withRouter } from '../common/with-router';
 
-const required = value => {
-    if (!value) {
-        return (
-            <div className="alert alert-danger" role="alert">
-                This field is required!
-            </div>
-        );
-    }
-};
 
 class Login extends Component {
     constructor(props) {
@@ -51,33 +39,25 @@ class Login extends Component {
             loading: true
         });
 
-        this.form.validateAll();
+        AuthService.login(this.state.username, this.state.password).then(
+            () => {
+                this.props.router.navigate("/profile");
+                window.location.reload();
+            },
+            error => {
+                const resMessage =
+                    (error.response &&
+                        error.response.data &&
+                        error.response.data.message) ||
+                    error.message ||
+                    error.toString();
 
-        if (this.checkBtn.context._errors.length === 0) {
-            AuthService.login(this.state.username, this.state.password).then(
-                () => {
-                    this.props.router.navigate("/profile");
-                    window.location.reload();
-                },
-                error => {
-                    const resMessage =
-                        (error.response &&
-                            error.response.data &&
-                            error.response.data.message) ||
-                        error.message ||
-                        error.toString();
-
-                    this.setState({
-                        loading: false,
-                        message: resMessage
-                    });
-                }
-            );
-        } else {
-            this.setState({
-                loading: false
-            });
-        }
+                this.setState({
+                    loading: false,
+                    message: resMessage
+                });
+            }
+        );
     }
     render() {
         return (
@@ -89,7 +69,7 @@ class Login extends Component {
                         className="profile-img-card"
                     />
 
-                    <Form
+                    <form
                         onSubmit={this.handleLogin}
                         ref={c => {
                             this.form = c;
@@ -97,25 +77,25 @@ class Login extends Component {
                     >
                         <div className="form-group">
                             <label htmlFor="username">Username</label>
-                            <Input
+                            <input
                                 type="text"
                                 className="form-control"
                                 name="username"
                                 value={this.state.username}
                                 onChange={this.onChangeUsername}
-                                validations={[required]}
+                                required
                             />
                         </div>
 
                         <div className="form-group">
                             <label htmlFor="password">Password</label>
-                            <Input
+                            <input
                                 type="password"
                                 className="form-control"
                                 name="password"
                                 value={this.state.password}
                                 onChange={this.onChangePassword}
-                                validations={[required]}
+                                required
                             />
                         </div>
 
@@ -138,13 +118,13 @@ class Login extends Component {
                                 </div>
                             </div>
                         )}
-                        <CheckButton
+                        <button
                             style={{ display: "none" }}
                             ref={c => {
                                 this.checkBtn = c;
                             }}
                         />
-                    </Form>
+                    </form>
                 </div>
             </div>
         );
--- frontend/src/components/register.component.js
+++ frontend/src/components/register.component.js
@@ -1,49 +1,6 @@
 import React, { Component } from "react";
-import Form from "react-validation/build/form";
-import Input from "react-validation/build/input";
-import CheckButton from "react-validation/build/button";
-import { isEmail } from "validator";
 
 import AuthService from "../services/auth.service";
-
-const required = value => {
-    if (!value) {
-        return (
-            <div className="alert alert-danger" role="alert">
-                This field is required!
-            </div>
-        );
-    }
-};
-
-const email = value => {
-    if (!isEmail(value)) {
-        return (
-            <div className="alert alert-danger" role="alert">
-                This is not a valid email.
-            </div>
-        );
-    }
-};
-
-const vusername = value => {
-    if (value.length < 3 || value.length > 20) {
-        return (
-            <div className="alert alert-danger" role="alert">
-                The username must be between 3 and 20 characters.
-            </div>
-        );
-    }
-};
-const vpassword = value => {
-    if (value.length < 6 || value.length > 40) {
-        return (
-            <div className="alert alert-danger" role="alert">
-                The password must be between 6 and 40 characters.
-            </div>
-        );
-    }
-};
 
 export default class Register extends Component {
     constructor(props) {
@@ -87,35 +44,32 @@ export default class Register extends Component {
             successful: false
         });
 
-        this.form.validateAll();
+        AuthService.register(
+            this.state.username,
+            this.state.email,
+            this.state.password
+        ).then(
+            response => {
+                this.setState({
+                    message: response.data.message,
+                    successful: true
+                });
+            },
+            error => {
+                const resMessage =
+                    (error.response &&
+                        error.response.data &&
+                        error.response.data.message) ||
+                    error.message ||
+                    error.toString();
 
-        if (this.checkBtn.context._errors.length === 0) {
-            AuthService.register(
-                this.state.username,
-                this.state.email,
-                this.state.password
-            ).then(
-                response => {
-                    this.setState({
-                        message: response.data.message,
-                        successful: true
-                    });
-                },
-                error => {
-                    const resMessage =
-                        (error.response &&
-                            error.response.data &&
-                            error.response.data.message) ||
-                        error.message ||
-                        error.toString();
+                this.setState({
+                    successful: false,
+                    message: resMessage
+                });
+            }
+        );
 
-                    this.setState({
-                        successful: false,
-                        message: resMessage
-                    });
-                }
-            );
-        }
     }
     render() {
         return (
@@ -127,7 +81,7 @@ export default class Register extends Component {
                         className="profile-img-card"
                     />
 
-                    <Form
+                    <form
                         onSubmit={this.handleRegister}
                         ref={c => {
                             this.form = c;
@@ -137,37 +91,37 @@ export default class Register extends Component {
                             <div>
                                 <div className="form-group">
                                     <label htmlFor="username">Username</label>
-                                    <Input
+                                    <input
                                         type="text"
                                         className="form-control"
                                         name="username"
                                         value={this.state.username}
                                         onChange={this.onChangeUsername}
-                                        validations={[required, vusername]}
+                                        required
                                     />
                                 </div>
 
                                 <div className="form-group">
                                     <label htmlFor="email">Email</label>
-                                    <Input
-                                        type="text"
+                                    <input
+                                        type="email"
                                         className="form-control"
                                         name="email"
                                         value={this.state.email}
                                         onChange={this.onChangeEmail}
-                                        validations={[required, email]}
+                                        required
                                     />
                                 </div>
 
                                 <div className="form-group">
                                     <label htmlFor="password">Password</label>
-                                    <Input
+                                    <input
                                         type="password"
                                         className="form-control"
                                         name="password"
                                         value={this.state.password}
                                         onChange={this.onChangePassword}
-                                        validations={[required, vpassword]}
+                                        required
                                     />
                                 </div>
 
@@ -191,13 +145,13 @@ export default class Register extends Component {
                                 </div>
                             </div>
                         )}
-                        <CheckButton
+                        <button
                             style={{ display: "none" }}
                             ref={c => {
                                 this.checkBtn = c;
                             }}
                         />
-                    </Form>
+                    </form>
                 </div>
             </div>
         );
