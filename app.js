464339d179d0ec50ed17ec1dfc1ecf4c06ffefc9
andreyrise <andrey.rise@gmail.com>
2022-10-29 22:01:21 -0700

    make it better

--- app.js
+++ app.js
@@ -91,10 +91,10 @@ app.get("/", (req, res) => {
 require('./routes/auth.routes')(app);
 require('./routes/user.routes')(app);
 // set port, listen for requests
-app.use('/', express.static(path.join(__dirname, 'frontend', 'build')))
+app.use('/', express.static(path.join(__dirname, 'client', 'build')))
 
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
 })
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
