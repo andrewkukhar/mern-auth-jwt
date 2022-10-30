419d0286f7153acaebed2b7947207fb9c7ce12eb
andreyrise <andrey.rise@gmail.com>
2022-10-29 21:39:21 -0700

    change again

--- app.js
+++ app.js
@@ -91,13 +91,11 @@ app.get("/", (req, res) => {
 require('./routes/auth.routes')(app);
 require('./routes/user.routes')(app);
 // set port, listen for requests
-if (process.env.NODE_ENV === 'production') {
-    app.use('/', express.static(path.join(__dirname, 'frontend', 'build')))
+app.use('/', express.static(path.join(__dirname, 'frontend', 'build')))
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-    })
-}
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+})
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
